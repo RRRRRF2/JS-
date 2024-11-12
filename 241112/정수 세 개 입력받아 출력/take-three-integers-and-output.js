a390b6ff2ab732078be3746f146f061f89ca61d0
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-11-12 22:55:48 +0900

    Update take-three-integers-and-output.js

--- 241112/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 241112/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString();
 
-arr = input.split(/\s+/);
+arr = input.split(/\s+/);  //정규표현식 \s+  ->  공백,탭,줄바꿈 등을 모두 포함
 
 let a = Number(arr[0]);
 let b = Number(arr[1]);
