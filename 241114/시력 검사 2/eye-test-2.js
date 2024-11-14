d25871038b262b8898392ee7ac12abc9d7eb5e13
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-11-14 15:55:51 +0900

    Title: 시력 검사 2, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241114/시력 검사 2/eye-test-2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString().trim())
+
+if(input >=1.0){
+    console.log("High");
+}
+else if(input >=0.5){
+    console.log("Middle");
+}
+else {
+    console.log("Low")
+}
