d0b7226712bdddcf3700c762351b03744177c5b6
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-11-27 18:43:47 +0900

    Title: 숫자의 계절은, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

--- 241127/숫자의 계절은/season-of-num.js
+++ 241127/숫자의 계절은/season-of-num.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 let a = Number(fs.readFileSync(0).toString().trim());
 
 if(3<= a && a <= 5) console.log("Spring");
-else if(6<= a && a <= 7) console.log("Summer");
+else if(6<= a && a <= 8) console.log("Summer");
 else if(9<= a && a <= 11) console.log("Fall");
 else if(a == 1 || a == 2 || a == 12) console.log("Winter");
