cb5b34491573a4cf0308d94efcdad4b61ad99985
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-11-29 21:36:04 +0900

    Title: 윤년인가, Time: 180ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241129/윤년인가/is-leap-year.js
+++ 241129/윤년인가/is-leap-year.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 let y = Number(fs.readFileSync(0).toString().trim());
 
-if(y % 100 == 0){
-    if(y % 4 ==0 && y % 400 !==0) console.log("false"); 
+if(y % 4 ==0){
+    console.log("true");
 }
-if(y%4==0) console.log("true");
+else{
+    if(y%100 ==0 && y% 400 !=0){
+        console.log("false");
+    }
+}
