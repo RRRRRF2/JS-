c744558558083d2901a8ab5bef1c0dc83cf93a4c
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-11-29 23:48:20 +0900

    Title: 코로나 메뉴얼, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 241129/코로나 메뉴얼/covid-manual.js
+++ 241129/코로나 메뉴얼/covid-manual.js
@@ -1,24 +1,28 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(/\s+/);
+let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let a = input[0];
-let a1 = Number(input[1]);
-let b = input[2];
-let b1 = Number(input[3]);
-let c = input[4];
-let c1 = Number(input[5]);
+let a = input[0].split(" ");
+let b = input[1].split(" ");
+let c = input[2].split(" ");
 
-let A =0; 
+let as = a[0] , at = Number(a[1]);
+let bs = b[0] , bt = Number(b[1]);
+let cs = c[0] , ct = Number(c[1]);
 
-if(a == "Y" && a1 >=37){
-    A +=1;
+if(as === "Y" && at >= 37){
+    if(bs === "Y" && bt >= 37 || cs === "Y" && ct >= 37){
+        console.log("E");
+    }
+    else{
+        console.log("N");
+    }
 }
-if(b == "Y" && b1 >=37){
-    A +=1;
-}
-if(c == "Y" && c1 >=37){
-    A +=1;
+else{
+    if((bs === "Y" && bt >= 37) && (cs === "Y" && ct >= 37)){
+        console.log("E");
+    }
+    else{
+        console.log("N");
+    }
 }
 
-if(A >=2) console.log("E");
-else console.log("N");
