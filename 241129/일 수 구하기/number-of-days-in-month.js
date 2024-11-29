6844bec23acdfa54a4d01c171359265cab0ba5b2
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-11-29 22:03:18 +0900

    Title: 일 수 구하기, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241129/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if(a == 1) console.log(31);
+else if(a == 2) console.log(28);
+else if(a == 3) console.log(31);
+else if(a == 4) console.log(33);
+else if(a == 5) console.log(31);
+else if(a == 6) console.log(30);
+else if(a == 7) console.log(31);
+else if(a == 8) console.log(31);
+else if(a == 9) console.log(30);
+else if(a == 10) console.log(31);
+else if(a == 11) console.log(30);
+else if(a == 12) console.log(31);
