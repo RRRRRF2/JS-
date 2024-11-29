83f192730c9d1c6e5a4912d0b4d9374a7a5390d6
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-11-29 22:12:12 +0900

    Title: 일 수 구하기, Time: 199ms, Memory: 15MB, Status: Passed - CodeTree

--- 241129/일 수 구하기/number-of-days-in-month.js
+++ 241129/일 수 구하기/number-of-days-in-month.js
@@ -1,15 +1,10 @@
 const fs = require("fs");
-let a = Number(fs.readFileSync(0).toString().trim());
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-if(a == 1) console.log(31);
-else if(a == 2) console.log(28);
-else if(a == 3) console.log(31);
-else if(a == 4) console.log(30);
-else if(a == 5) console.log(31);
-else if(a == 6) console.log(30);
-else if(a == 7) console.log(31);
-else if(a == 8) console.log(31);
-else if(a == 9) console.log(30);
-else if(a == 10) console.log(31);
-else if(a == 11) console.log(30);
-else if(a == 12) console.log(31);
+let y = Number(fs.readFileSync(0).toString().trim());
+
+function getDaysInMonth(month) {
+    console.log(daysInMonth[month - 1]);
+}
+
+getDaysInMonth(y);
