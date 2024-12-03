0922015a63b4420388f5d1e865bdc48541164ac5
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-12-04 00:30:04 +0900

    Title: 문자열 출력하기, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

--- 241204/문자열 출력하기/print-string.js
+++ 241204/문자열 출력하기/print-string.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let n = Number(fs.readFileSync(0).toString().trim);
+let n = Number(fs.readFileSync(0).toString().trim());
 
 for(let i=0; i<n; i++){
     console.log("LeebrosCode");
