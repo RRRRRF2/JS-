1a9be97f9a7ab12e8a3fa06750e9bbdfdb17db26
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-12-04 00:11:11 +0900

    Title: 짝수만 출력 2, Time: 171ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241204/짝수만 출력 2/output-only-even-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let b = Number(input[0]);
+let a = Number(input[1]);
+
+let result = "";
+
+while(b>=a){
+    if(b%2==0){
+        result += b + " ";
+    }
+    b--;
+}
+console.log(result);
