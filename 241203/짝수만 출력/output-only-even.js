eecb1999567b898481dec78a34e9591d95132417
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-12-03 19:16:22 +0900

    Title: 짝수만 출력, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241203/짝수만 출력/output-only-even.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let result = "";
+
+while(a<=b){
+    if(a%2 ==0){
+    result += a + " ";
+    }
+    a++;
+}
+console.log(result);
