0a82882dcb6c1225db929430f332db311a849186
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-12-06 01:27:44 +0900

    Title: 자연수면 출력, Time: 188ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241206/자연수면 출력/output-only-natural-number.js
+++ 241206/자연수면 출력/output-only-natural-number.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
 let result = "";
-if(a % 1 ===0 && a>0){
+if(Number.isInteger(a) && a>0){
     for(let i = 1; i<=b; i++){
         result += a;
     }
