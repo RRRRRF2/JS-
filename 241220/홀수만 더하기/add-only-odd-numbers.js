9ac943dec5e8a3f661cb5efea2f7226c3cdeff79
정윤호 <101507515+RRRRRF2@users.noreply.github.com>
2024-12-20 23:24:44 +0900

    Title: 홀수만 더하기, Time: 183ms, Memory: 18MB, Status: Passed - CodeTree

--- 241220/홀수만 더하기/add-only-odd-numbers.js
+++ 241220/홀수만 더하기/add-only-odd-numbers.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
-let n = fs.readFileSync(0).toString().trim().split(/\s+/);
+let input = fs.readFileSync(0).toString().trim().split(/\s+/);
 
 
+let n = Number(input[0]);
 let result =0;
 
 for(let i =1; i<=n; i++){
-    let x = n[i];
+    let x = Number(input[i]);
     if(x%2 ==1 && x%3 ==0){
         result += x;
     }
